8320a43c2aa477117c6a40bdebf77181e09c74b6
brurodri712 <brunorodrigues.p54@gmail.com>
2023-05-29 20:50:50 -0300

    29/05/2023

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Apostila_003 from './components/apostila-003';
+import Apostila_004 from './components/apostila-004';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Apostila_003 />
+      <Apostila_004 />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +18,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    //paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#9E9E9E',
+  //paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#FFF',
     padding: 8,
   },
 });
